2e6cd00f664af6d970c43ff2f39bdfb6a163d35c
Jonathan Jontarciego Santos <34256647+jjsantos1@users.noreply.github.com>
2018-05-29 16:50:30 +0800

    final push

--- connect.php
+++ connect.php
@@ -1,10 +1,23 @@
 <?php
 //Connection to MySQL server
+$hostname = 'sql113.byethost.com';
+$username = 'b16_22153224';
+$password = 'atinglat';
+$database = 'b16_22153224_subject';
 
-$hostname = 'localhost';
-$username = 'jjsantos1';
-$password = '';
-$database = 'subject';
-
+if(isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+	$_SESSION['username'] = $username;
+	$_SESSION['password'] = $password;
+	$link = mysqli_connect($hostname,$_SESSION['username'],$_SESSION['password'],$database);
+	
+    if($link) {
+        header('location:homepage.php');          
+        }
+		else{
+			header('location:login.php');
+		}
+}
 $link = mysqli_connect($hostname,$username,$password,$database);
 ?>
--- login.php
+++ login.php
@@ -1,39 +1,17 @@
-<?php
-session_start();
-?>
 <html>
 <head>
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="gga.css">
 </head>
 <body>
-<a href="homepage.php">Home</a> <br />
-<?php
-include("connect.php");
- 
-if(isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-	$_SESSION['username'] = $username;
-	$_SESSION['password'] = $password;
-
-    if($link) {
-        header('location:homepage.php');          
-        }
-    }
-else {
-?>
 <div>
 <p>Enter your login credentials here.</p>
-<form method="POST" action="">
+<form method="POST" action="../connect.php">
 Username<input type="text" name="username"><br>
 Password<input type="text" name="password">
 <input type="submit" name="submit" value="Enter">
 <input type="reset" name= "reset" value="Clear">
 </form>
 </div>
-<?php
-}
-?>
 </body>
 </html>
